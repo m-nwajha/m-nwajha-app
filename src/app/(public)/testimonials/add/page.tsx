5d348f8a07555b555aad78c8bbde8142248bb8e3
m-nwajha <m.nawjha@gmail.com>
2026-05-11 15:45:05 +0300

    feat: update home page metadata title and add new testimonial

--- src/app/(public)/testimonials/add/page.tsx
+++ src/app/(public)/testimonials/add/page.tsx
@@ -1,5 +1,10 @@
 import AddTestimonialPage from '@/components/pages/AddTestimonialPage';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+    title: 'إضافة تقييم جديد',
+    description: 'إضافة تقييم جديد Nawjha Tech',
+}
 export default function AddTestimonial() {
     return <AddTestimonialPage />
 }
