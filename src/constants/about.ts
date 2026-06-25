0ce888d9c4b3e1021275058d40e0adea486d3eb6
m-nwajha <m.nawjha@gmail.com>
2026-06-25 12:24:21 +0300

    add a new photo

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -3,6 +3,6 @@ export const ABOUT_DATA= {
     description: 'مهندس برمجيات متكامل (Full-Stack) بخبرة 3 سنوات في JavaScript وTypeScript. متخصص في بناء تطبيقات ويب قوية وقابلة للتوسع من البداية إلى النهاية باستخدام React وNext.js وExpress.js وMongoDB. هدفي مساعدة العملاء على تحويل الأفكار المعقدة إلى حلول رقمية نظيفة، سهلة الصيانة، وملائمة للمستخدم.',
     name: 'محمد النواجحة',
     job: 'مهندس برمجيات',
-    image: '/assets/images/mohamed-photo.png',
+    image: '/assets/images/nwajha-photo.png',
     buttonA: { href: '#', label: 'السيرة الذاتية' },
 }
